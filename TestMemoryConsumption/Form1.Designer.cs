6c0a840b5d30329681b69c1a1ddb7c5261802690
Alexander Petrovskiy <alexander.petrovskiy@devexpress.com>
2020-12-15 23:14:59 +0300

    #TestMemoryConsumption project

---
+++ TestMemoryConsumption/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TestMemoryConsumption {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.tablePanel1 = new DevExpress.Utils.Layout.TablePanel();
+            this.stackPanel2 = new DevExpress.Utils.Layout.StackPanel();
+            this.stackPanel1 = new DevExpress.Utils.Layout.StackPanel();
+            this.seriesTypeComboBox = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.pointsCountComboBox = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.seriesCreationTypeComboBox = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.resultLabel = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.tablePanel1)).BeginInit();
+            this.tablePanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stackPanel2)).BeginInit();
+            this.stackPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stackPanel1)).BeginInit();
+            this.stackPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesTypeComboBox.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pointsCountComboBox.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesCreationTypeComboBox.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablePanel1
+            // 
+            this.tablePanel1.Columns.AddRange(new DevExpress.Utils.Layout.TablePanelColumn[] {
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 10F)});
+            this.tablePanel1.Controls.Add(this.stackPanel2);
+            this.tablePanel1.Controls.Add(this.stackPanel1);
+            this.tablePanel1.Controls.Add(this.chartControl1);
+            this.tablePanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tablePanel1.Location = new System.Drawing.Point(0, 0);
+            this.tablePanel1.Name = "tablePanel1";
+            this.tablePanel1.Rows.AddRange(new DevExpress.Utils.Layout.TablePanelRow[] {
+            new DevExpress.Utils.Layout.TablePanelRow(DevExpress.Utils.Layout.TablePanelEntityStyle.Absolute, 26F),
+            new DevExpress.Utils.Layout.TablePanelRow(DevExpress.Utils.Layout.TablePanelEntityStyle.Absolute, 26F)});
+            this.tablePanel1.Size = new System.Drawing.Size(800, 450);
+            this.tablePanel1.TabIndex = 0;
+            // 
+            // stackPanel2
+            // 
+            this.tablePanel1.SetColumn(this.stackPanel2, 0);
+            this.stackPanel2.Controls.Add(this.resultLabel);
+            this.stackPanel2.Location = new System.Drawing.Point(3, 3);
+            this.stackPanel2.Name = "stackPanel2";
+            this.tablePanel1.SetRow(this.stackPanel2, 0);
+            this.stackPanel2.Size = new System.Drawing.Size(661, 20);
+            this.stackPanel2.TabIndex = 2;
+            // 
+            // stackPanel1
+            // 
+            this.tablePanel1.SetColumn(this.stackPanel1, 1);
+            this.stackPanel1.Controls.Add(this.seriesTypeComboBox);
+            this.stackPanel1.Controls.Add(this.pointsCountComboBox);
+            this.stackPanel1.Controls.Add(this.seriesCreationTypeComboBox);
+            this.stackPanel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.stackPanel1.LayoutDirection = DevExpress.Utils.Layout.StackPanelLayoutDirection.TopDown;
+            this.stackPanel1.Location = new System.Drawing.Point(667, 3);
+            this.stackPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.stackPanel1.Name = "stackPanel1";
+            this.tablePanel1.SetRow(this.stackPanel1, 0);
+            this.tablePanel1.SetRowSpan(this.stackPanel1, 2);
+            this.stackPanel1.Size = new System.Drawing.Size(133, 444);
+            this.stackPanel1.TabIndex = 1;
+            // 
+            // seriesTypeComboBox
+            // 
+            this.seriesTypeComboBox.Location = new System.Drawing.Point(1, 3);
+            this.seriesTypeComboBox.Name = "seriesTypeComboBox";
+            this.seriesTypeComboBox.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.seriesTypeComboBox.Size = new System.Drawing.Size(130, 20);
+            this.seriesTypeComboBox.TabIndex = 4;
+            this.seriesTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.SeriesTypeComboBox_SelectedIndexChanged);
+            // 
+            // pointsCountComboBox
+            // 
+            this.pointsCountComboBox.Location = new System.Drawing.Point(1, 29);
+            this.pointsCountComboBox.Name = "pointsCountComboBox";
+            this.pointsCountComboBox.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.pointsCountComboBox.Size = new System.Drawing.Size(130, 20);
+            this.pointsCountComboBox.TabIndex = 5;
+            this.pointsCountComboBox.SelectedIndexChanged += new System.EventHandler(this.PointsCountComboBox_SelectedIndexChanged_1);
+            // 
+            // chartControl1
+            // 
+            this.tablePanel1.SetColumn(this.chartControl1, 0);
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Location = new System.Drawing.Point(3, 29);
+            this.chartControl1.Name = "chartControl1";
+            this.tablePanel1.SetRow(this.chartControl1, 1);
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl1.Size = new System.Drawing.Size(661, 418);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // seriesCreationTypeComboBox
+            // 
+            this.seriesCreationTypeComboBox.Location = new System.Drawing.Point(1, 55);
+            this.seriesCreationTypeComboBox.Name = "seriesCreationTypeComboBox";
+            this.seriesCreationTypeComboBox.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.seriesCreationTypeComboBox.Size = new System.Drawing.Size(130, 20);
+            this.seriesCreationTypeComboBox.TabIndex = 6;
+            this.seriesCreationTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.SeriesCreationTypeComboBox_SelectedIndexChanged_1);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Location = new System.Drawing.Point(3, 3);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tablePanel1);
+            this.Name = "Form1";
+            this.Text = "ChartControl Memory Consumption";
+            ((System.ComponentModel.ISupportInitialize)(this.tablePanel1)).EndInit();
+            this.tablePanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.stackPanel2)).EndInit();
+            this.stackPanel2.ResumeLayout(false);
+            this.stackPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stackPanel1)).EndInit();
+            this.stackPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.seriesTypeComboBox.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pointsCountComboBox.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesCreationTypeComboBox.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.Utils.Layout.TablePanel tablePanel1;
+        private DevExpress.Utils.Layout.StackPanel stackPanel1;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private DevExpress.Utils.Layout.StackPanel stackPanel2;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.ComboBoxEdit seriesTypeComboBox;
+        private DevExpress.XtraEditors.ComboBoxEdit pointsCountComboBox;
+        private DevExpress.XtraEditors.ComboBoxEdit seriesCreationTypeComboBox;
+        private DevExpress.XtraEditors.LabelControl resultLabel;
+    }
+}
+
---
+++ TestMemoryConsumption/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+
+namespace TestMemoryConsumption {
+    public partial class Form1 : Form {
+        const int MB = 1024 * 1024; 
+
+        Series series;
+        double memoryBeforeDataLoading;
+        bool isChartPreparing = false;
+        Dictionary<ViewType, int> seriesViewValueCount;
+
+        public Form1() {
+            InitializeComponent();
+            InitializeSeriesViewValueDict();
+            InitializeChart();
+            InitializeSeriesTypes();
+            InitializePointsCount();
+            InitializeSeriesCreationType();
+            this.timer1.Start();
+        }
+        void InitializeSeriesViewValueDict() {
+            this.seriesViewValueCount = new Dictionary<ViewType, int>();
+            this.seriesViewValueCount.Add(ViewType.Area, 1);
+            this.seriesViewValueCount.Add(ViewType.BoxPlot, 7);
+            this.seriesViewValueCount.Add(ViewType.Bubble, 2);
+            this.seriesViewValueCount.Add(ViewType.CandleStick, 4);
+            this.seriesViewValueCount.Add(ViewType.FullStackedArea, 1);
+            this.seriesViewValueCount.Add(ViewType.FullStackedBar, 1);
+            this.seriesViewValueCount.Add(ViewType.FullStackedSplineArea, 1);
+            this.seriesViewValueCount.Add(ViewType.Line, 1);
+            this.seriesViewValueCount.Add(ViewType.RangeBar, 2);
+            this.seriesViewValueCount.Add(ViewType.Point, 1);
+            this.seriesViewValueCount.Add(ViewType.ScatterLine, 1);
+            this.seriesViewValueCount.Add(ViewType.SideBySideStackedBar, 1);
+            this.seriesViewValueCount.Add(ViewType.SideBySideRangeBar, 2);
+            this.seriesViewValueCount.Add(ViewType.SideBySideFullStackedBar, 1);
+            this.seriesViewValueCount.Add(ViewType.SplineArea, 1);
+            this.seriesViewValueCount.Add(ViewType.StackedBar, 1);
+            this.seriesViewValueCount.Add(ViewType.StackedSplineArea, 1);
+            this.seriesViewValueCount.Add(ViewType.StepLine, 1);
+            this.seriesViewValueCount.Add(ViewType.Stock, 4);
+            this.seriesViewValueCount.Add(ViewType.Waterfall, 1);
+            this.seriesViewValueCount.Add(ViewType.Bar, 1);
+        }
+        void InitializeSeriesCreationType() {
+            this.seriesCreationTypeComboBox.Properties.Items.Add(DataCreationMode.SeriesDataSource);
+            this.seriesCreationTypeComboBox.Properties.Items.Add(DataCreationMode.Manual);
+            this.seriesCreationTypeComboBox.Properties.Items.Add(DataCreationMode.ChartDataSource);
+            this.seriesCreationTypeComboBox.SelectedIndex = 0;
+        }
+        void InitializeChart() {
+            this.chartControl1.CrosshairEnabled = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+
+        }
+        void InitializeSeriesTypes() {
+            object[] viewTypes = new object[] {
+                ViewType.Area, ViewType.BoxPlot, ViewType.Bubble, ViewType.CandleStick, ViewType.FullStackedArea,
+                ViewType.FullStackedBar, ViewType.FullStackedSplineArea, ViewType.Line, ViewType.RangeBar, ViewType.Point,
+                ViewType.ScatterLine, ViewType.Bar, ViewType.SideBySideStackedBar, ViewType.SideBySideFullStackedBar,
+                ViewType.SideBySideRangeBar, ViewType.SplineArea, ViewType.StackedArea, ViewType.StackedBar, ViewType.StackedSplineArea,
+                ViewType.StepLine, ViewType.Stock, ViewType.Waterfall
+            };
+            this.seriesTypeComboBox.Properties.Items.AddRange(viewTypes);
+            this.seriesTypeComboBox.SelectedItem = ViewType.Line;
+        }
+        void InitializePointsCount() {
+            this.pointsCountComboBox.Properties.Items.AddRange(new object[5] { 1000, 10000, 100000, 1000000, 10000000 });
+            this.pointsCountComboBox.SelectedIndex = 3;
+        }
+        void RecreateSeries() {
+            if(this.seriesCreationTypeComboBox.SelectedItem == null)
+                return;
+            if((DataCreationMode)this.seriesCreationTypeComboBox.SelectedItem == DataCreationMode.Manual)
+                RecreateManualCreatedSeries();
+            else
+                RecreateBindingSeries();
+        }
+        void RecreateBindingSeries() {
+            List<DataItem> dataSource = DataItem.GenerateDataSource((int)this.pointsCountComboBox.SelectedItem);
+            Prepare();
+            this.series.ValueDataMembers.AddRange(DataItem.GetValueMembers(this.seriesViewValueCount[(ViewType)this.seriesTypeComboBox.SelectedItem]));
+            this.series.ArgumentDataMember = "Argument";
+
+            if((DataCreationMode)this.seriesCreationTypeComboBox.SelectedItem == DataCreationMode.SeriesDataSource)
+                this.series.DataSource = dataSource;
+            else
+                this.chartControl1.DataSource = dataSource;
+        }
+        void RecreateManualCreatedSeries() {
+            Prepare();
+            SeriesPoint[] points = DataItem.CreateSeriesPoints((int)this.pointsCountComboBox.SelectedItem);
+            this.series.Points.AddRange(points);
+        }
+        void Prepare() {
+            this.isChartPreparing = true;
+            this.chartControl1.DataSource = null;
+            this.chartControl1.Series.Clear();
+            this.series = new Series();
+            this.chartControl1.Series.Add(this.series);
+            this.series.ChangeView((ViewType)this.seriesTypeComboBox.SelectedItem);
+            this.chartControl1.Update();
+            this.memoryBeforeDataLoading = GetTotalMemory();
+            this.isChartPreparing = false;
+        }
+        void UpdateMemoryUsage() {
+            if(!this.isChartPreparing) {
+                double totalMemory = GetTotalMemory();
+                double resultMemory = totalMemory - this.memoryBeforeDataLoading;
+                this.resultLabel.Text = string.Format("Series Memory {0:0.00} MB Total Memory {1:0.00} MB Before Memory {2:0.00}", resultMemory, totalMemory,  this.memoryBeforeDataLoading);
+            }
+        }
+        double GetTotalMemory() {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            return (double)GC.GetTotalMemory(true) / MB;
+        }
+        void Timer1_Tick(object sender, EventArgs e) {
+            UpdateMemoryUsage();
+        }
+
+        void SeriesTypeComboBox_SelectedIndexChanged(object sender, EventArgs e) {
+            RecreateSeries();
+        }
+
+        void PointsCountComboBox_SelectedIndexChanged_1(object sender, EventArgs e) {
+            RecreateSeries();
+        }
+
+        void SeriesCreationTypeComboBox_SelectedIndexChanged_1(object sender, EventArgs e) {
+            RecreateSeries();
+        }
+    }
+
+    public enum DataCreationMode {
+        Manual, SeriesDataSource, ChartDataSource
+    }
+
+    public class DataItem {
+        public static List<DataItem> GenerateDataSource(int pointCount) {
+            List<DataItem> dataSource = new List<DataItem>();
+            for(int i = 0; i < pointCount; ++i)
+                dataSource.Add(new DataItem(i, i - 1, i, i + 1, i + 2, i + 3, i + 4, i + 5));
+            return dataSource;
+        }
+        public static SeriesPoint[] CreateSeriesPoints(int pointsCount) {
+            SeriesPoint[] result = new SeriesPoint[pointsCount];
+            for(int i = 0; i < pointsCount; ++i)
+                result[i] = new SeriesPoint(i, i - 1, i, i + 1, i + 2, i + 3, i + 4, i + 5);
+            return result;
+        }
+        public static string[] GetValueMembers(int n) {
+            string[] valueNames = new string[n];
+            for(int i = 0; i < n; i++)
+                valueNames[i] = "Value" + (i + 1).ToString();
+            return valueNames;
+        }
+
+        public double Argument { get; }
+        public double Value1 { get; }
+        public double Value2 { get; }
+        public double Value3 { get; }
+        public double Value4 { get; }
+        public double Value5 { get; }
+        public double Value6 { get; }
+        public double Value7 { get; }
+
+        public DataItem(double argument, double value1, double value2, double value3, double value4, double value5, double value6, double value7) {
+            Argument = argument;
+            Value1 = value1;
+            Value2 = value2;
+            Value3 = value3;
+            Value4 = value4;
+            Value5 = value5;
+            Value6 = value6;
+            Value7 = value7;
+        }
+    }
+}
